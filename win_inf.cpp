52851f6d488aafa50a63eb98c11cf16bbf448f36
shnfi <mrplom7@gmail.com>
2025-02-15 19:39:25 +0330

    replaces 'lan_messenger' with the name of the program

--- win_inf.cpp
+++ win_inf.cpp
@@ -1,13 +1,13 @@
 struct IP_WINDOW_INF
 {
-    const QString WIN_TITLE = "LAN Messenger - Enter IP";
+    const QString WIN_TITLE = "Local Hub - Enter IP";
     const int WIN_W = 588;
     const int WIN_H = 350;
 } IP_WIN_INF;
 
 struct CHAT_WINDOW_INF
 {
-    const QString WIN_TITLE = "LAN Messenger - Chat";
+    const QString WIN_TITLE = "Local Hub - Chat";
     const int WIN_W = 588;
     const int WIN_H = 727;
 } CHAT_WIN_INF;
