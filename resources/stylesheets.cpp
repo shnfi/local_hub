a864ae4a7866731f73826099a5d444ca84d1628a
shnfi <mrplom7@gmail.com>
2025-02-18 21:29:25 +0330

    (fundumental structure conversion) moved the stylesheet code to the 'resources' directory

---
+++ resources/stylesheets.cpp
@@ -0,0 +1,68 @@
+typedef const QString ss;
+
+ss dialog_ss = R"(background-color: #FFFFFF;)";
+
+ss ip_field_ss = R"(border-radius: 22px;
+                    background-color: #DFDFDF;
+                    height: 50px;
+                    padding: 0 15px;
+                    color: #000000;)";
+
+ss submit_button_ss = R"(QPushButton {
+                        border-radius: 22px;
+                        background-color: #2294FB;
+                        height: 50px;
+                    }
+                    QPushButton::hover {
+                        background-color: #1e89e6;
+                    })";
+
+ss exit_button_ss = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #FF605C;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(227, 82, 80);
+                    })";
+
+ss maxmize_button_ss = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #00CA4E;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(0, 170, 65);
+                    })";
+
+ss minimize_button_ss = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #FFBD44;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(230, 170, 59);
+                    })";
+
+ss title_bar_ss = R"(background-color: #EDEDED;)";
+
+ss title_label_ss = R"(color: #000000;)";
+
+ss ip_displayer_widget_ss = R"(#ip_displayer_widget {
+                            background-color: #DFDFDF;
+                            border-bottom: 1px solid #BABABA;
+                        })";
+
+ss ip_label_ss = R"(color: #000000;)";
+
+ss chat_container_widget_ss = R"(background-color: #FFFFFF;)";
+
+ss message_field_container_widget_ss = R"(background-color: #FFFFFF;)";
+
+ss message_field_ss = R"(background-color: #DFDFDF;
+                        color: #000000;
+                        padding: 0 15px;
+                        border: none;
+                        border-radius: 20px;)";
+
+ss send_button_ss = R"(background-color: #64FC4C;
+                        color: #000000;
+                        border: none;
+                        border-radius: 20px;)";
