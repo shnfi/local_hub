2b2c579970efef3477bc2318aa579ad983f68c57
shnfi <mrplom7@gmail.com>
2025-02-18 21:31:19 +0330

    (fundumental structure conversion) removed the chat_window class from the 'main.cpp' and renamed it to window

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
-#include <iostream>
-
-#include <QApplication>
-#include <QWidget>
-#include <QStackedWidget>
-#include <QDialog>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QLabel>
-#include <QSpacerItem>
-#include <QTimer>
-#include <QScrollArea>
-
-#include "chat_scr.cpp"
+#include "src/window.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-	Chat_Window chat_win;
-	chat_win.show();
+	Window win;
+	win.show();
 
 	return app.exec();
 }
