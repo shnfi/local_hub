6fa40ea50f296970a2cb00d247fe3373d43d91a7
shnfi <mrplom7@gmail.com>
2025-03-05 19:58:50 +0330

    new stylesheets for different themes of the application

---
+++ resources/dark_stylesheet.cpp
@@ -0,0 +1,82 @@
+ss dialog_ss_dark = R"(background-color: transparent;)";
+
+ss frame_ss_dark = R"(background-color: #1C1C1E;
+                border-radius: 15px;)";
+
+ss ip_field_ss_dark = R"(border-radius: 20px;
+                    background-color: #3A3A3C;
+                    height: 50px;
+                    padding: 0 15px;
+                    color: #FFFFFF;)";
+
+ss submit_button_ss_dark = R"(QPushButton {
+                        border-radius: 20px;
+                        background-color: #2294FB;
+                        height: 50px;
+                    }
+                    QPushButton::hover {
+                        background-color: #1e89e6;
+                    })";
+
+ss exit_button_ss_dark = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #FF605C;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(227, 82, 80);
+                    })";
+
+ss maxmize_button_ss_dark = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #00CA4E;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(0, 170, 65);
+                    })";
+
+ss minimize_button_ss_dark = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #FFBD44;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(230, 170, 59);
+                    })";
+
+ss title_bar_ss_dark = R"(background-color: #3A3A3C;
+                    border-top-right-radius: 15px;
+                    border-top-left-radius: 15px;)";
+
+ss title_label_ss_dark = R"(color: #FFFFFF;)";
+
+ss ip_displayer_widget_ss_dark = R"(#ip_displayer_widget {
+                            background-color: #3A3A3C;
+                            border-bottom: 1px solid gray;
+                        })";
+
+ss ip_label_ss_dark = R"(color: #FFFFFF;)";
+
+ss chat_container_widget_ss_dark = R"(background-color: #1C1C1E;
+                                border-radius: 15px;)";
+
+ss message_field_container_widget_ss_dark = R"(background-color: #1C1C1E;
+                                        border-radius: 15px;)";
+
+ss message_field_ss_dark = R"(background-color: #3A3A3C;
+                        color: #FFFFFF;
+                        padding: 0 15px;
+                        border: none;
+                        border-radius: 20px;)";
+
+ss send_button_ss_dark = R"(background-color: #2294FB;
+                        border: none;
+                        border-radius: 20px;)";
+
+ss sending_message_widget_ss_dark = R"(background-color: #2294FB;
+                                border-radius: 20px;
+                                padding: 0 5px;
+                            )";
+ss received_message_widget_ss_dark = R"(background-color: #3A3A3C;
+                                    color: #FFFFFF;
+                                    border-radius: 20px;
+                                    padding: 0 5px;
+                                )";
---
+++ resources/light_stylesheet.cpp
@@ -0,0 +1,84 @@
+typedef const QString ss;
+
+ss dialog_ss_light = R"(background-color: transparent;)";
+
+ss frame_ss_light = R"(background-color: #FFFFFF;
+                border-radius: 15px;)";
+
+ss ip_field_ss_light = R"(border-radius: 20px;
+                    background-color: #EDEDED;
+                    height: 50px;
+                    padding: 0 15px;
+                    color: #000000;)";
+
+ss submit_button_ss_light = R"(QPushButton {
+                        border-radius: 20px;
+                        background-color: #2294FB;
+                        height: 50px;
+                    }
+                    QPushButton::hover {
+                        background-color: #1e89e6;
+                    })";
+
+ss exit_button_ss_light = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #FF605C;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(227, 82, 80);
+                    })";
+
+ss maxmize_button_ss_light = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #00CA4E;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(0, 170, 65);
+                    })";
+
+ss minimize_button_ss_light = R"(QPushButton {
+                        border-radius: 7px;
+                        background-color: #FFBD44;
+                    }
+                    QPushButton::hover {
+                        background-color:rgb(230, 170, 59);
+                    })";
+
+ss title_bar_ss_light = R"(background-color: #EDEDED;
+                    border-top-right-radius: 15px;
+                    border-top-left-radius: 15px;)";
+
+ss title_label_ss_light = R"(color: #000000;)";
+
+ss ip_displayer_widget_ss_light = R"(#ip_displayer_widget {
+                            background-color: #EDEDED;
+                            border-bottom: 1px solid #BABABA;
+                        })";
+
+ss ip_label_ss_light = R"(color: #000000;)";
+
+ss chat_container_widget_ss_light = R"(background-color: #FFFFFF;
+                                border-radius: 15px;)";
+
+ss message_field_container_widget_ss_light = R"(background-color: #FFFFFF;
+                                        border-radius: 15px;)";
+
+ss message_field_ss_light = R"(background-color: #EDEDED;
+                        color: #000000;
+                        padding: 0 15px;
+                        border: none;
+                        border-radius: 20px;)";
+
+ss send_button_ss_light = R"(background-color: #2294FB;
+                        border: none;
+                        border-radius: 20px;)";
+
+ss sending_message_widget_ss_light = R"(background-color: #2294FB;
+                                border-radius: 20px;
+                                padding: 0 5px;
+                            )";
+ss received_message_widget_ss_light = R"(background-color: #EDEDED;
+                                    color: #000000;
+                                    border-radius: 20px;
+                                    padding: 0 5px;
+                                )";
