ef5e83ea019aed41ba6e12789d7a8bb816b9c31c
shnfi <mrplom7@gmail.com>
2025-03-11 16:58:00 +0330

    window information for the network connection error dialog

--- win_inf.cpp
+++ win_inf.cpp
@@ -11,3 +11,10 @@ struct CHAT_WINDOW_INF
     const int WIN_W = 588;
     const int WIN_H = 727;
 } CHAT_WIN_INF;
+
+struct NETERR_WINDOW_INF
+{
+    const QString WIN_TITLE = "Local Hub - Network Error";
+    const int WIN_W = 300;
+    const int WIN_H = 300;
+} NETERR_WIN_INF;
